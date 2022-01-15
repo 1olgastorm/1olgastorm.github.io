d2919f2c1c128edbb72bc184ab5ea4b5db487360
1olgastorm <97412106+1olgastorm@users.noreply.github.com>
2022-01-15 10:20:34 +0000

    Add files via upload

---
+++ pointerLock.js
@@ -0,0 +1,9 @@
+  let createPointerLock = function(scene) {
+  let canvas = scene.getEngine().getRenderingCanvas();
+  canvas.addEventListener("click", event => {
+    canvas.requestPointerLock = canvas.requestPointerLock || canvas.msRequestPointerLock || canvas.mozRequestPointerLock || canvas.webkitRequestPointerLock;
+    if(canvas.requestPointerLock) {
+      canvas.requestPointerLock();
+    }
+  }, false);
+}; 
